d533a9c3b9a7853051c386dad153de11bc599e08
Russell Tian <russell.tian1996@gmail.com>
2018-07-09 14:57:33 -0400

    good prefix tree question

--- leetcode/leetcode/Unusual_Idea.hpp
+++ leetcode/leetcode/Unusual_Idea.hpp
@@ -20,4 +20,77 @@ int singleNumber(vector<int>& nums);//136,single number, easy, bitwise manipulat
 
 ListNode *detectCycle(ListNode *head); //142,linked list cycle II, mid, interesting math
 
+
+// Design
+class TrieNode{
+    /*
+     each trieNode has a size 26 prefix list,
+     and a bool to find out if this word is a inserted word, or a prefix
+     drawback: larger memory
+     */
+public:
+    TrieNode *child[26]; // next prefix list
+    bool isWord; // is this a inserted word
+    TrieNode():isWord(false){
+        for(int i = 0; i < 26; i++){
+            child[i] = NULL;
+        }
+    }
+    
+};
+class Trie {
+public:
+    /** Initialize your data structure here. */
+    
+    Trie() {
+        root = new TrieNode();
+        
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        //if not inserted, create a new node and mark the last character as isWord
+        TrieNode *temp = root; //first location
+        for(int i = 0; i < word.size(); i++){
+            int j = word[i] - 'a'; // the index that should be inserted
+            if(!temp->child[j]) temp->child[j] = new TrieNode(); // create one
+            // cout <<word[i] <<' '<< j<<endl;
+            temp = temp->child[j];
+        }
+        temp->isWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        //if find the word and isWord is true, yes
+        TrieNode *temp = root;
+        for(int i = 0; i < word.size();i++){
+            int j = word[i] - 'a';
+            if(!temp->child[j]) return false;
+            temp = temp->child[j];
+        }
+        if(temp->isWord)return true;
+        else return false;
+        
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        //find the prefix
+        TrieNode *temp = root;
+        for(int i = 0; i < prefix.size();i++){
+            int j = prefix[i] - 'a';
+            if(!temp->child[j]) return false;
+            temp = temp->child[j];
+        }
+        return true;
+    }
+    
+private:
+    TrieNode *root;
+};
+//208 implement trie (prefix tree), mid-trie
+//very good question, how dictionary works
+
+
 #endif /* Unusual_Idea_hpp */
