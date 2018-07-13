ed13faf8293ff0a0a060266cfbefb33b89618c57
Russell Tian <russell.tian1996@gmail.com>
2018-07-12 22:06:36 -0400

    hard dp

--- leetcode/leetcode/DP.cpp
+++ leetcode/leetcode/DP.cpp
@@ -181,6 +181,40 @@ int lengthOfLIS(vector<int>& nums) {
 }
 
 /*
+309. Best Time to Buy and Sell Stock with Cooldown
+ Say you have an array for which the ith element is the price of a given stock on day i.
+ 
+ Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+ 
+ You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+ Example:
+ 
+ Input: [1,2,3,0,2]
+ Output: 3
+ Explanation: transactions = [buy, sell, cooldown, buy, sell]
+ */
+int maxProfit_cd(vector<int>& prices) {
+    /*
+     upgrade version to #121
+     */
+    if(prices.empty()||prices.size()==1)return 0;
+    
+    vector<int> profit(prices.size(),0);//max profit if sell it today
+    vector<int> buy(prices.size(),0);//max profit if buy it today
+    buy[0]=-prices[0];buy[1]=max(buy[0],-prices[1]); //
+    profit[0]=0;profit[1]=max(0,buy[0]+prices[1]);
+    
+    
+    for(int i = 2; i< prices.size();i++){
+        buy[i] = max(profit[i-2]-prices[i],buy[i-1]);//buy or hold
+        profit[i] = max(profit[i-1],buy[i-1]+prices[i]);//hold or sell
+        //  cout<<i<<": "<<buy[i]<<' '<<profit[i]<<endl;
+    }
+    return profit[prices.size()-1];
+}
+
+/*
  Greedy Algorithm
  */
 /*
--- leetcode/leetcode/DP.hpp
+++ leetcode/leetcode/DP.hpp
@@ -20,6 +20,7 @@ bool wordBreak(string s, vector<string>& wordDict); // 139, word break, mid, nee
 int maxProduct(vector<int>& nums);//152, max product subarray, mid
 int maximalSquare(vector<vector<char>>& matrix); //221. Maximal Square, mid
 int lengthOfLIS(vector<int>& nums); //300. Longest Increasing Subsequence,mid, O(n^2)need to be O(nlogn)
+int maxProfit_cd(vector<int>& prices);//309. Best Time to Buy and Sell Stock with Cooldown, mid hard to me, need more thinking
 
 /*
  Greedy Algorithm
