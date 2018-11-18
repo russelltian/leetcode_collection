5d2934c8428f11556473212accfcc8d44b64e9cb
russelltian <russell.tian1996@gmail.com>
2018-11-18 12:48:17 -0500

    upate

--- leetcode/leetcode/Tree.cpp
+++ leetcode/leetcode/Tree.cpp
@@ -427,8 +427,41 @@ TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
     return t2;
 }
 
+/*
+108. Convert Sorted Array to Binary Search Tree
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
 
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
 
+Example:
+
+Given the sorted array: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+	  0
+	 / \
+   -3   9
+   /   /
+ -10  5
+*/
+TreeNode* sortedArrayToBST(vector<int>& nums) {
+	if (nums.size() == 0)return NULL;
+	int l = 0; int r = int(nums.size()) - 1;
+	return sortedArrayToBST(nums, l, r);
+
+}
+TreeNode* sortedArrayToBST(vector<int>& nums, int l, int r) {
+	//more thinking why l > r is termination condition
+	int m = (l + r) / 2;
+	if (l > r) {
+		return NULL;
+	}
+	TreeNode* root = new TreeNode(nums[m]);
+	root->left = sortedArrayToBST(nums, l, m - 1);
+	root->right = sortedArrayToBST(nums, m + 1, r);
+	return root;
+}
 
 /*
  general helper function
