7fa10965047272c761e458c372c8896ae729a18e
Hongyi Tian <russell.tian1996@gmail.com>
2019-02-24 01:25:15 -0700

    hard working day

--- leetcode/leetcode/Unusual_Idea.cpp
+++ leetcode/leetcode/Unusual_Idea.cpp
@@ -175,38 +175,30 @@ bool isHappy(int n) {
 238. Product of Array Except Self
 Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
 
-Example:
-
 Input:  [1,2,3,4]
 Output: [24,12,8,6]
 Note: Please solve it without division and in O(n).
  */
 vector<int> productExceptSelf(vector<int>& nums) {
-    /*
-     original vec: 1  2  3  4
-     after:        0  1  2  6
-     before:       24 12 4  0
-     after[last], before[0]  not change
-     the rest after[i]*before[i] = ans 24 12 8 6
-     faster than 99.3%
-     */
-    vector<int> ans(nums.size(),0);
-    ans[1]=nums[0];
-    for(int i = 2; i < nums.size(); i++){
-        ans[i] = nums[i-1]*ans[i-1];
-    }
-    int size = int(nums.size());
-    vector<int> before(size,0);
-    before[size-2] = nums[size-1];
-    for(int i = size-3; i >=0; i--){
-        before[i] = nums[i+1]*before[i+1];
-    }
-    ans[0] = before[0];
-    for(int i = 1; i < nums.size()-1; i++){
-        ans[i]=ans[i]*before[i];
-        //cout<<ans[i];
-    }
-    return ans;
+       /*
+       original vec: 1  2  3  4
+       after:        1  1  2  6
+       before:       24 12 4  1
+       after[i]*before[i] = ans 24 12 8 6
+
+       */
+       int size = nums.size();
+       vector<int> after(size,1);//left to right
+       vector<int> before(size,1);//right to left
+       for(int i = 1; i < size;i++){
+           after[i] = after[i-1]*nums[i-1];
+       }
+       for(int i = size-2;i >= 0; i--){
+           before[i] = before[i+1]*nums[i+1];
+           nums[i+1]=before[i+1]*after[i+1];
+       }
+       nums[0]=before[0]*after[0];
+       return nums;
 }
 
 /*
