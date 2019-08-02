045f446a04881d38ba082dbea70576183d2f93d2
russelltian <russell.tian1996@gmail.com>
2019-08-02 15:02:27 -0400

    73 set matrix zeroes

---
+++ leetcode/Array/SaveMemory/73.SetMatrixZeros/README.md
@@ -0,0 +1,20 @@
+## Description 
+
+Given a *m* x *n* matrix, if an element is 0, set its entire row and column to 0. Do it [**in-place**](https://en.wikipedia.org/wiki/In-place_algorithm).
+
+````
+Example 1:
+
+
+````
+````
+Example 2:
+
+
+````
+## Solution
+Complete Times:  <br/>
+Difficulty:  <br/>
+Time complexity: O()<br/>
+Space complexity: O()<br/>
+
---
+++ leetcode/Array/SaveMemory/73.SetMatrixZeros/main.cpp
@@ -0,0 +1,37 @@
+void setZeroes(vector<vector<int>>& matrix) {
+	// we can easily come up with a O(m+n) space complexity solution
+	if (matrix.size() == 0)return;
+	bool isrow = false; bool iscol = false;
+	for (int i = 0; i < matrix.size(); i++) {
+		for (int j = 0; j < matrix[0].size(); j++) {
+			if (matrix[i][j] == 0) {
+				matrix[0][j] = 0; matrix[i][0] = 0;
+				if (i == 0)isrow = true; if (j == 0)iscol = true;
+			}
+		}
+	}
+	for (int i = 1; i < matrix.size(); i++) {
+		if (matrix[i][0] == 0) {
+			for (int j = 0; j < matrix[i].size(); j++) {
+				matrix[i][j] = 0;
+			}
+		}
+	}
+	for (int i = 1; i < matrix[0].size(); i++) {
+		if (matrix[0][i] == 0) {
+			for (int j = 0; j < matrix.size(); j++) {
+				matrix[j][i] = 0;
+			}
+		}
+	}
+	if (isrow) {
+		for (int i = 0; i < matrix[0].size(); i++) {
+			matrix[0][i] = 0;
+		}
+	}
+	if (iscol) {
+		for (int i = 0; i < matrix.size(); i++) {
+			matrix[i][0] = 0;
+		}
+	}
+}
